b76ea65358588d717db2b24f6492b7c92b30e83e
msreeves <reevesy87@hotmail.co.uk>
2023-12-31 19:30:17 +0000

    version 1.0

--- inc/components/event.php
+++ inc/components/event.php
@@ -17,7 +17,7 @@
                                 <div class="carousel-item <?php echo ($count == 0) ? 'active' : ''; ?>">
                                    <div class="row g-0">
                                     <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $slide->ID ), 'single-post-thumbnail' ); ?>
-    <div class="p-5 background-image" style="background-image: url('<?php echo $image[0]; ?>');">
+    <div class="background-image" style="background-image: url('<?php echo $image[0]; ?>');">
   <div class="mask p-5" style="background-color: rgba(0, 0, 0, 0.2);">
     <div class="d-flex justify-content-center align-items-center h-100">
       <div class="text-white text-center">
@@ -30,7 +30,7 @@
          <h3><?php if($date['start']) : ?>  <i class="fa-solid fa-calendar"></i> <?php endif; ?> <?php echo $date['start']; ?> <?php if($date['finish']) : ?> - <?php echo $date['finish']; ?><?php endif; ?> </h3> 
             <?php $time = get_field('time' , $slide->ID); ?>
           <h3><?php if($time['start']) : ?>  <i class="fa-solid fa-clock"></i> <?php endif; ?> <?php echo $time['start']; ?> <?php if($time['finish']) : ?> - <?php echo $time['finish']; ?><?php endif; ?> </h3> 
-         <div class="d-flex justify-content-around">
+         <div class="ctas">
                                         <?php 
 $link = get_field('link1', $slide->ID);
 if( $link ): 
