ccfc39d481eaff05261774d09e083e3f6bff3efe
msreeves <reevesy87@hotmail.co.uk>
2026-08-30 19:39:44 +0100

    fix categories

--- inc/setup.php
+++ inc/setup.php
@@ -26,3 +26,8 @@ function msrevents_theme_setup() {
 	add_theme_support( 'html5', array( 'search-form', 'gallery', 'caption', 'style', 'script' ) );
 }
 add_action( 'after_setup_theme', 'msrevents_theme_setup' );
+
+/**
+ * Drop "Category:", "Tag:", etc. if archive titles use get_the_archive_title().
+ */
+add_filter( 'get_the_archive_title_prefix', '__return_empty_string' );
